1386b226693b3cd3149ef457f87255766995a2c3
Peter Stadler <stadlerpeter@yahoo.fr>
2024-09-09 18:17:39 +0200

    fix module location

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cat <<EOF > /usr/local/apache2/conf/extra/vife.conf
 LoadModule	dav_module           modules/mod_dav.so
 LoadModule	dav_svn_module       /usr/lib/apache2/modules/mod_dav_svn.so
 LoadModule	authz_svn_module     /usr/lib/apache2/modules/mod_authz_svn.so
-LoadModule  auth_openidc_module  modules/mod_auth_openidc.so
+LoadModule  auth_openidc_module  /usr/lib/apache2/modules/mod_auth_openidc.so
 
 # rewriting Destination because we're behind an SSL terminating reverse proxy
 # see http://www.dscentral.in/2013/04/04/502-bad-gateway-svn-copy-reverse-proxy/  
