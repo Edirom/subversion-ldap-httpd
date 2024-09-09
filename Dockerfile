1f120d5ce441bd0634984876896e7351b987bdee
Peter Stadler <stadlerpeter@yahoo.fr>
2024-09-09 18:05:18 +0200

    updates for openidc

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,17 @@ FROM httpd:2.4
 LABEL maintainer="Peter Stadler for the ViFE"
 
 # For information about these parameters see 
-# https://httpd.apache.org/docs/2.4/mod/mod_authnz_ldap.html
-ARG AuthLDAPURL
-ARG AuthLDAPBindDN
-ARG AuthLDAPBindPassword
-ARG RequireLDAPGroup
+# https://github.com/OpenIDC/mod_auth_openidc
+ARG OIDCProviderMetadataURL
+ARG OIDCClientID
+ARG OIDCClientSecret
+ARG OIDCRedirectURI
+ARG OIDCCryptoPassphrase
 
-RUN apt-get update && apt-get --yes --force-yes --no-install-recommends install curl subversion libapache2-mod-svn \
-    && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && \
+    apt-get install --no-install-recommends -y curl subversion ca-certificates libapache2-mod-auth-openidc && \
+    rm -rf /var/lib/apt/lists/* \
+
 COPY entrypoint.sh /my-docker-entrypoint.sh
 RUN chmod 755 /my-docker-entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,30 +5,30 @@ cat <<EOF > /usr/local/apache2/conf/extra/vife.conf
 LoadModule	dav_module           modules/mod_dav.so
 LoadModule	dav_svn_module       /usr/lib/apache2/modules/mod_dav_svn.so
 LoadModule	authz_svn_module     /usr/lib/apache2/modules/mod_authz_svn.so
-LoadModule	ldap_module          modules/mod_ldap.so
-LoadModule	authnz_ldap_module   modules/mod_authnz_ldap.so
+LoadModule  auth_openidc_module  modules/mod_auth_openidc.so
 
 # rewriting Destination because we're behind an SSL terminating reverse proxy
 # see http://www.dscentral.in/2013/04/04/502-bad-gateway-svn-copy-reverse-proxy/  
 RequestHeader edit Destination ^https: http: early
 
+OIDCProviderMetadataURL ${OIDCProviderMetadataURL}
+OIDCClientID ${OIDCClientID}
+OIDCClientSecret ${OIDCClientSecret}
+
+# OIDCRedirectURI is a vanity URL that must point to a path protected by this module but must NOT point to any content
+OIDCRedirectURI ${OIDCRedirectURI}
+OIDCCryptoPassphrase <password>
+
+# maps the preferred_username claim to the REMOTE_USER environment variable
+OIDCRemoteUserClaim preferred_username
+
 <Location />
     DAV svn
     SVNParentPath /var/svn
-    SVNListParentPath On
-    AuthName "ViFE Subversion Repositories: Login mit Nutzernamen und Passwort" 
-    AuthBasicProvider ldap
-    AuthType Basic
-    AuthLDAPGroupAttribute member
-    AuthLDAPGroupAttributeIsDN on
-    AuthLDAPURL ${AuthLDAPURL}
-    AuthLDAPBindDN "${AuthLDAPBindDN}" 
-    AuthLDAPBindPassword "${AuthLDAPBindPassword}" 
-    Require ldap-group CN=${RequireLDAPGroup},CN=Users,DC=muwi,DC=hfm-detmold,DC=de
-    # read-only access
-    <limit GET PROPFIND OPTIONS HEAD REPORT>
-        Require ldap-user redmine
-    </limit>
+    SVNListParentPath Onx
+
+    AuthType openid-connect
+    Require valid-user
 </Location>
 EOF
 
